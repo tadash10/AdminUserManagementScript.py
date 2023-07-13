043f096d2e15c0f5ffc1989be5fffe7bbf1345a2
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-12 23:12:12 -0300

    Update Change user password.py

--- Change user password.py
+++ Change user password.py
@@ -1,1 +1,10 @@
+def change_password(user_id, new_password):
+    url = f"{API_BASE_URL}/users/{user_id}/password"
+    data = {'new_password': new_password}
+    response = send_request(url, method='PUT', data=data)
+    
+    if response is not None and response.status_code == 200:
+        print("Password changed successfully!")
+    else:
+        print("Failed to change password.")
 
